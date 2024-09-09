20bf5b62b7674ec577eddba8e3a8d9a9307a39b7
datajoely <joel.schwarzmann@aneira.health>
2024-09-09 11:50:01 +0100

    update models + gitgignore
    Signed-off-by: datajoely <joel.schwarzmann@aneira.health>

--- temp_dbt/models/gold/encounter_descriptive_stats.sql
+++ temp_dbt/models/gold/encounter_descriptive_stats.sql
@@ -1,24 +1,43 @@
+WITH joined_data AS (
+    SELECT
+        te.id AS encounter_id,
+        DATE_TRUNC(
+            'year',
+            START
+        ) AS date_year,
+        encounterclass,
+        base_encounter_cost,
+        procedures_cnt,
+        medications_cnt,
+        conditions_cnt
+    FROM
+        {{ source(
+            'raw',
+            'ENCOUNTERS'
+        ) }}
+        te
+        INNER JOIN {{ ref('count_procedures') }}
+        tp
+        ON te.id = tp.id
+        INNER JOIN {{ ref('count_conditions') }}
+        tc
+        ON te.id = tc.id
+        INNER JOIN {{ ref('count_medications') }}
+        tm
+        ON te.id = tm.id
+)
 SELECT
-    te.id,
-    START,
-    stop,
+    date_year,
     encounterclass,
-    base_encounter_cost,
-    procedures_cnt,
-    medications_cnt,
-    conditions_cnt
+    COUNT(
+        DISTINCT encounter_id
+    ) AS encounters,
+    SUM(base_encounter_cost) AS encounter_cost,
+    SUM(procedures_cnt) AS procedures_cnt,
+    SUM(medications_cnt) AS medications_cnt,
+    SUM(conditions_cnt) AS conditions_cnt
 FROM
-    {{ source(
-        'raw',
-        'ENCOUNTERS'
-    ) }}
-    te
-    INNER JOIN {{ ref('count_procedures') }}
-    tp
-    ON te.id = tp.id
-    INNER JOIN {{ ref('count_conditions') }}
-    tc
-    ON te.id = tc.id
-    INNER JOIN {{ ref('count_medications') }}
-    tm
-    ON te.id = tm.id
+    joined_data
+GROUP BY
+    date_year,
+    encounterclass
--- temp_dbt/models/silver/count_medications.sql
+++ temp_dbt/models/silver/count_medications.sql
@@ -1,3 +1,3 @@
-SELECT ENCOUNTER as Id, count(DISTINCT code) as medications_cnt
+SELECT ENCOUNTER as Idw, count(DISTINCT code) as medications_cnt
 FROM {{ source('raw', 'MEDICATIONS') }}
 GROUP BY ENCOUNTER, CODE
